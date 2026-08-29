ffb5d568c0956bb19192a94401da4d38523efbf8
kabartey <121442056+kabartey@users.noreply.github.com>
2026-08-29 11:15:46 +0300

    chore(config): configure Firebase and app bundle ID
    Update Android and iOS build configurations, set the package name to com.kabartey.ilactakip, and integrate Firebase services.

---
+++ android/app/src/main/java/com/kabartey/ilactakip/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kabartey.ilactakip;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/kabartey/ilactakip/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kabartey.ilactakip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
